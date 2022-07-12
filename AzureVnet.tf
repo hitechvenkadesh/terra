d1edba14b4e9058abf2c8ec16a4bfc4ddda15228
hitechvenkadesh <48478948+hitechvenkadesh@users.noreply.github.com>
2022-07-12 09:04:11 +0530

    Update AzureVnet.tf

--- AzureVnet.tf
+++ AzureVnet.tf
@@ -3,10 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "3.13.0"
     }
   }
 }
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
