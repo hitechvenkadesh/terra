5a9ebdf0aef507969707a79fc092dd091ebfc648
hitechvenkadesh <48478948+hitechvenkadesh@users.noreply.github.com>
2022-12-14 21:04:20 +0530

    Update newinfra.tf

--- newinfra.tf
+++ newinfra.tf
@@ -112,14 +112,14 @@ resource "random_id" "randomId" {
     byte_length = 8
 }
 
-resource "azurerm_storage_account" "mystorageaccount" {
-    name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = azurerm_resource_group.myterraformgroup.name
-    location                    = "eastus"
-    account_replication_type    = "LRS"
-    account_tier                = "Standard"
+# resource "azurerm_storage_account" "mystorageaccount" {
+#     name                        = "diag${random_id.randomId.hex}"
+#     resource_group_name         = azurerm_resource_group.myterraformgroup.name
+#     location                    = "eastus"
+#     account_replication_type    = "LRS"
+#     account_tier                = "Standard"
 
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
+#     tags = {
+#         environment = "Terraform Demo"
+#     }
+# }
